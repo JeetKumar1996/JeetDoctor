7740eeaf578be97da6eb49f5c36c2bf3f7d57f6c
JeetKumar1996 <101659485+JeetKumar1996@users.noreply.github.com>
2022-04-16 21:11:06 +0545

    Add files via upload

---
+++ JS/Main.js
@@ -0,0 +1,29 @@
+/* .myNameTypingCenter {
+    display: flex;
+    justify-content:start;
+}
+
+.myNameTyping {
+    display: inline-block;
+    width: 31ch;
+    animation: typing 2s steps(22), blink .5s step-end infinite alternate;
+    white-space: nowrap;
+    overflow: hidden;
+    border-right: 3px solid;
+    font-family: monospace;
+    font-size: 2em;
+    color: white;
+    margin-top: 15px;
+    overflow: visible;
+} */
+/* @keyframes typing {
+    from {
+        width: 0
+    }
+}
+@keyframes blink {
+    50% {
+        border-color: transparent
+    }
+} */
+
